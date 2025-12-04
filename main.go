29e2c7df06d08d7cbf156eb83c2567fcd0f1ed1e
Julio Chrisostomo <julio.chrisostomo@gmail.com>
2025-12-04 00:18:33 -0300

    feat: add job running in intervals

--- main.go
+++ main.go
@@ -1,28 +1,54 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"time"
 
 	"github.com/jchrisos/pingenemy/internal/http"
 	"github.com/jchrisos/pingenemy/internal/url"
 )
 
 func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
 	url := &url.UrlRequest{
 		Name:               "google",
 		URL:                "https://google.com",
 		HttpMethod:         "GET",
 		ExpectedStatusCode: 200,
+		IntervalSeconds:    5,
 	}
+
+	go executeUrl(ctx, url)
+
+	select {}
+}
+
+func executeUrl(ctx context.Context, urlReq *url.UrlRequest) {
+	interval := time.Duration(urlReq.IntervalSeconds) * time.Second
+	ticker := time.NewTicker(interval)
+	defer ticker.Stop()
 
 	exec := &http.HttpExecutor{}
 
-	success, err := exec.Execute(url)
-	if err != nil {
-		panic("Failed to calling url")
-	}
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("Job is done")
+			return
+		case <-ticker.C:
+			func() {
+				success, err := exec.Execute(urlReq)
+				if err != nil {
+					panic("Failed to calling url")
+				}
 
-	if success {
-		fmt.Println("OK")
+				if success {
+					fmt.Println("OK")
+				}
+			}()
+		}
 	}
 }
